6d76edfe9e4f2fab9d22e6ee174d5beda96a7dc4
Matt Ramotar <matt.ramotar@uber.com>
2024-10-20 19:21:14 -0400

    Remove Quota Guard
    Signed-off-by: Matt Ramotar <matt.ramotar@uber.com>

--- backend/server/src/main/kotlin/org/mobilenativefoundation/trails/backend/server/Hikari.kt
+++ backend/server/src/main/kotlin/org/mobilenativefoundation/trails/backend/server/Hikari.kt
@@ -2,13 +2,11 @@ package org.mobilenativefoundation.trails.backend.server
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
-import java.net.URI
 
 internal fun createHikariDataSource(): HikariDataSource {
     val connection = System.getenv("CONNECTION")
     val username = System.getenv("USERNAME")
     val password = System.getenv("PASSWORD")
-    val proxyUrl = URI(System.getenv("QUOTAGUARDSHIELD_URL"))
 
     val hikariConfig = HikariConfig().apply {
         this.jdbcUrl = connection
@@ -16,7 +14,6 @@ internal fun createHikariDataSource(): HikariDataSource {
         this.password = password
         driverClassName = "org.postgresql.Driver"
         maximumPoolSize = 10
-        connectionInitSql = "SET SESSION socks_proxy_host='${proxyUrl.host}', socks_proxy_port='${proxyUrl.port}'"
     }
 
     return HikariDataSource(hikariConfig)
