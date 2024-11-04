f002a3dcc6f645649490228792f7405c76f67dc9
Matt Ramotar <matt.ramotar@uber.com>
2024-11-04 11:36:32 -0500

    Make output serializable
    Signed-off-by: Matt Ramotar <matt.ramotar@uber.com>

--- xplat/lib/models/src/commonMain/kotlin/org/mobilenativefoundation/trails/xplat/lib/models/post/PostOutput.kt
+++ xplat/lib/models/src/commonMain/kotlin/org/mobilenativefoundation/trails/xplat/lib/models/post/PostOutput.kt
@@ -1,8 +1,15 @@
 package org.mobilenativefoundation.trails.xplat.lib.models.post
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 sealed class PostOutput {
-    data class Keys(val values: List<Post.Key>): PostOutput()
-    data class Key(val value: Post.Key): PostOutput()
+    data class Keys(val values: List<Post.Key>) : PostOutput()
+    data class Key(val value: Post.Key) : PostOutput()
+
+    @Serializable
     data class Single(val value: Post) : PostOutput()
+
+    @Serializable
     data class Collection(val values: List<Post>) : PostOutput()
 }
